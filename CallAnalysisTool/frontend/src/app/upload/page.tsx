9b7569e9d814ec935c019907a8eba317f9535afd
camdenlaskie <camden.laskie@gmail.com>
2025-11-20 13:13:22 -0600

    fix file names

--- CallAnalysisTool/frontend/src/app/upload/page.tsx
+++ CallAnalysisTool/frontend/src/app/upload/page.tsx
@@ -3,9 +3,9 @@ import UploadFileContainer from "@/components/uploadFileContainer";
 export default function Upload() {
   return (
     <>
-      <div className="flex flex-col items-center h-screen mt-10">
-        <h1 className="text-4xl font-bold mb-8">Dispatch Evaluation</h1>
-        <div className="w-full">
+      <div className="flex flex-col items-center h-screen overflow-hidden">
+        <h1 className="text-4xl font-bold mb-8 mt-10">Dispatch Evaluation</h1>
+        <div className="w-full flex-1 overflow-hidden flex flex-col">
           <UploadFileContainer />
         </div>
       </div>
